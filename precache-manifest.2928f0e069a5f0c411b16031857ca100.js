71ab0c904480a45e5e594fde97aa986754c73155
Eugene <lasttrip@ukr.net>
2020-08-27 19:13:50 +0300

    Updates

---
+++ precache-manifest.2928f0e069a5f0c411b16031857ca100.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b020b27e66ee309dbbd0776851c20700",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "f051c05f1a3204b6b4b8",
+    "url": "/weather-app/static/css/main.94886fe4.chunk.css"
+  },
+  {
+    "revision": "dddeb6b081fb5258e05d",
+    "url": "/weather-app/static/js/2.d58b7545.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.d58b7545.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f051c05f1a3204b6b4b8",
+    "url": "/weather-app/static/js/main.8a39c20c.chunk.js"
+  },
+  {
+    "revision": "3acee814d99e1bf21403",
+    "url": "/weather-app/static/js/runtime-main.e765d545.js"
+  },
+  {
+    "revision": "4711bdd37a980389a55079d80b2e83b0",
+    "url": "/weather-app/static/media/evening.4711bdd3.jpg"
+  }
+]);
